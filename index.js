fbf1f53e52858f5e26c7c1e44eee4e57f4f8a0de
Savannah C <sgcorrell@gmail.com>
2019-09-27 16:08:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const animal = "cat"
+const animal = "cat";
 
 function myAnimal() {
+  const animal = "dog";
   return animal
+}
 
-  function yourAnimal() {
-    const animal = "dog"
-    return animal
-  }
+function yourAnimal() {
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+  return animal
 }
 
 
